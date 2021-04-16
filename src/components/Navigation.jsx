331c4ab21164fd2b7570c3f45c5c5aab4b405c10
vincent <osf@hotmail.fr>
2021-04-16 10:13:12 +0200

    style: add divider to navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,10 +3,31 @@ import styled from 'styled-components';
 
 const Nav = styled.nav`
   align-items: center;
+  border-image: 
+    linear-gradient( 
+      to bottom, 
+      transparent,
+    #3a3a3c, 
+    rgba(252,252,252,0.2),
+    #3a3a3c ,
+      transparent 
+    ) 1 100%;
+  border-style: solid;
+  border-width: 0 0.2rem 0 0;
   display: flex;
   height: 100%;
   justify-content: center;
+  position: relative;
   width: max-content;
+`;
+
+const NavOutliner = styled.span`
+    background-color: ${(props) => props.theme.colors.violet};
+    height: 0.2rem;
+    left: 1.6rem;
+    margin-bottom: -0.2rem;
+    position: absolute;
+    width: 3rem;
 `;
 
 const NavLink = styled.a`
@@ -19,6 +40,7 @@ const Navigation = () => (
   <Nav>
     <NavLink>Dashboard</NavLink>
     <NavLink>Assets</NavLink>
+    <NavOutliner />
   </Nav>
 );
 
